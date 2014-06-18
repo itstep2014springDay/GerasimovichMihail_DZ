010a5511f2509340284a69c666e55e919d8392e3
Gerasimovich121212 <G_misha@tut.by>
2014-06-18 16:35:01 +0300

    dz

---
+++ dz3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=7, b=5;
+    printf("%d + %d =%d\n", a, b, a + b);
+    printf("%d - %d =%d\n", a, b, a - b);
+    printf("%d * %d =%d\n", a, b, a * b);
+    printf("%d / %d =%d\n", a, b, a / b);
+    printf("%d %% %d =%d\n", a, b, a % b);
+    return 0;
+}
